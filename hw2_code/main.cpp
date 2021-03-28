53e9512ec0c4d5aa9d548185c2cc39744505a4ee
Darowcat <jimmy890718@gmail.com>
2021-03-28 08:53:55 -0700

    all

---
+++ hw2_code/main.cpp
@@ -0,0 +1,132 @@
+// R = 9.54k
+// C = 47 * 10 ^ -9
+// cut-off freq = 1 / 2 * PI * R * C = 355
+
+#include "mbed.h"
+#include "uLCD_4DGL.h"
+
+using namespace std::chrono;
+
+InterruptIn up_button(A2);
+InterruptIn down_button(A0);
+InterruptIn confirm_button(A1);
+AnalogOut Aout(PA_4);
+AnalogIn Ain(A5);
+uLCD_4DGL uLCD(D1, D0, D2);
+
+//Timer timer;
+int freq = 355;
+double T = 1000.0f / freq;
+double i = 0.0f;
+double ADCdata[50];
+int j;
+bool conf = false;
+int idx = 0;
+//EventQueue queue(32 * EVENTS_EVENT_SIZE);
+//Thread thread;
+
+void info(){
+    uLCD.locate(1,2);
+    uLCD.printf("%d\n", freq);
+    uLCD.printf("%.2lf\n", T);
+}
+
+/*
+void add_freq(){
+    if (duration_cast<milliseconds>(timer.elapsed_time()).count() > 500)
+    {
+        freq += 50;
+        timer.reset();
+
+        queue.call(info);
+    }
+}
+
+void dec_freq(){
+    if (duration_cast<milliseconds>(timer.elapsed_time()).count() > 500)
+    {
+        freq -= 50;
+        timer.reset();
+
+        queue.call(info);
+    }
+}
+
+void confirm_freq(){
+    if (duration_cast<milliseconds>(timer.elapsed_time()).count() > 500)
+    {
+        T = 1000.0f / freq;
+        timer.reset();
+
+        queue.call(info);
+    }
+}
+*/
+
+int main()
+{
+    
+    
+    uLCD.text_width(2);
+    uLCD.text_height(2);
+    uLCD.color(WHITE);
+    info();
+    /*
+    timer.start();
+    up_button.rise(&add_freq);
+    down_button.rise(&dec_freq);
+    confirm_button.rise(&confirm_freq);
+
+    thread.start(callback(&queue, &EventQueue::dispatch_forever));
+    queue.call(info);
+    */
+   
+    while (1)
+    {
+        if (up_button == 1){
+            freq += 10;
+            info();
+        }
+
+        if (down_button == 1){
+            freq -= 10;
+            info();
+        }
+
+        if (confirm_button == 1){
+            T = 1000.0f / freq;
+            
+            conf = true;
+            info();
+        }
+        
+        if (i >= T){
+            i = 0;
+            idx = 0;
+        }
+        if (i <= 0.9f * T){
+            Aout = i / (0.99f * T);
+        } else {
+            Aout = (T - i) / (0.11f * T);
+        }
+
+        
+        //printf("%lf\r\n", Ain);
+        wait_us(int(T * 20.0f - 40.0f));
+        
+        i += (T / 50.0f);
+
+        if (conf){
+            ADCdata[idx] = Ain;
+            if (idx == 49){
+                break;
+            } else {
+                idx++;
+            }
+        }
+    }
+    printf("%d\r\n", freq);
+    for (j = 0; j < 50; j++){
+        printf("%lf\r\n", ADCdata[j]);
+    }
+}
